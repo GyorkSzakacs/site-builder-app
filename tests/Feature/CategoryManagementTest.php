e4bb14f5a85b4783cf66133a174f17c6cc0d2ff7
Szakacs Gyork <gyorkszakacs@gmail.com>
2022-01-25 17:04:54 +0100

    A user can create category

---
+++ tests/Feature/CategoryManagementTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\Category;
+
+class CategoryManagementTest extends TestCase
+{
+    
+    use RefreshDatabase;
+
+    /**
+     * Can a User create a Category
+     *
+     * @return void
+     */
+    public function test_a_user_can_create_category()
+    {
+        $this->withoutExceptionHandling();
+
+        $response = $this->post('/category',[
+            'tittle' => 'Home',
+            'position' => 1
+        ]);
+
+        $response->assertOk();
+
+        $this->assertCount(1, Category::all());
+    }
+}
