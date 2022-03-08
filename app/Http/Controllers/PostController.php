47e65c999dc6bfab07380aa2aa560e2771c6dd63
Szakacs Gyork <gyorkszakacs@gmail.com>
2022-03-08 10:15:28 +0100

    A post image can be uploaded

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Services\FileUploader\Uploader;
+use App\Services\FileUploader\ImageConstraints;
 
 class PostController extends Controller
 {
@@ -15,13 +17,19 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
+        $uploadedImage = $request->file('post_image');
+
+        $uploader = new Uploader($uploadedImage, new ImageConstraints());
+
+        $path = $uploader->upload();
+        
         Post::create([
             'title' => $request->title,
             'slug' => $request->slug,
             'title_visibility' => $request->title_visibility,
             'description' => $request->description,
             'content' => $request->content,
-            'post_image' => $request->post_image,
+            'post_image' => $path,
             'position' => $request->position,
             'section_id' => $request->section_id
         ]);
--- tests/Feature/PostManagementTest.php
+++ tests/Feature/PostManagementTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
 use App\Models\Post;
 
@@ -21,18 +22,23 @@ class PostManagementTest extends TestCase
     {
         $this->withoutExceptionHandling();
         
+        $image = UploadedFile::fake()->image('image.jpg');
+
         $response = $this->post('/post', [
             'title' => 'Első cikkem',
             'slug' => '',
             'title_visibility' => true,
             'description' => 'Az első cikkem.',
             'content' => 'Ez az első cikkem.',
-            'post_image' => 'post.jpg',
+            'post_image' => $image,
             'position' => 1,
             'section_id' => 1
         ]);
 
         $response->assertStatus(200);
         $this->assertCount(1, Post::all());
+
+        Storage::disk('local')->assertExists('images/'.$image->hashName());
+        $this->assertEquals('images/'.$image->hashName(), Post::first()->post_image);
     }
 }
