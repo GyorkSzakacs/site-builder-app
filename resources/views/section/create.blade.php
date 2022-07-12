67d5f5e4212182a1467d2b59b04dea12d35816c5
Szakacs Gyork <gyorkszakacs@gmail.com>
2022-07-12 17:57:28 +0200

    Create new section screen

--- resources/views/section/create.blade.php
+++ resources/views/section/create.blade.php
@@ -1,1 +1,64 @@
-{{$pageId}}
+<x-manager-layout>
+    <x-auth-card>
+
+    <div class="mb-4">
+            <h1 class="font-semibold text-2xl text-center">{{ __('Új szekció hozzáadása') }}</h1>
+        </div>
+
+        <x-slot name="logo">
+            <a href="/">
+                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+            </a>
+        </x-slot>
+
+        <!-- Validation Errors -->
+        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+
+        <form method="POST" action="/section">
+            @csrf
+
+            <!-- Title -->
+            <div>
+                <x-label for="title" :value="__('Cím')" />
+
+                <x-input id="title" class="block mt-1 w-full" type="text" name="title" :value="old('title')" required autofocus />
+            </div>
+
+            <!-- Title visibility-->
+            <div class="mt-4">
+                <x-label for="title_visibility" :value="__('Cím láthatósága')" />
+
+                <div class="flex ml-2">
+                    <x-input id="visibility_true" class="block mt-1" type="radio" name="title_visibility" :value=1 checked/>
+                    <x-label for="visibility_true" class="block p-1 text-xs" :value="__('Igen')" />
+                </div>
+
+                <div class="flex ml-2">
+                    <x-input id="visibility_false" class="block mt-1" type="radio" name="title_visibility" :value=0 />
+                    <x-label for="visibility_false" class="block p-1 text-xs" :value="__('Nem')" />
+                </div>
+            </div>
+
+            <!-- Position -->
+            <div class="mt-4">
+                <x-label for="position" :value="__('Pozíció')" />
+
+                <x-input id="position" class="block mt-1" type="number" name="position" :value="old('position')" min="1" max="10" required />
+            </div>
+
+            <input type="hidden" name="page_id" value="{{ $pageId }}"/>
+
+            <div class="flex items-center justify-end mt-4">
+
+                <x-cancel :link="url()->previous()">
+                    {{ __('Mégse') }}
+                </x-cancel>
+                
+                <x-button class="ml-4">
+                    {{ __('Hozzáadás') }}
+                </x-button>
+
+            </div>
+        </form>
+    </x-auth-card>
+</x-guest-layout>
