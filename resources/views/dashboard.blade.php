489407359a13f538dcc4f3f10c0129d95e6cf2a3
Szakacs Gyork <gyorkszakacs@gmail.com>
2022-07-27 13:51:21 +0200

    Authorized user management on the dashboard scrren

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,7 +5,8 @@
         </h1>
     </x-slot>
 
-    <div class="py-12">
+    @can('viewAny', App\Models\User::class)
+    <div class="pt-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
@@ -74,7 +75,9 @@
             </div>
         </div>
     </div>
-    <div class="pb-12">
+    @endcan
+
+    <div class="p-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
