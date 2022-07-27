fc6f1b9d761faea643425dab2001edb7de701fbc
Szakacs Gyork <gyorkszakacs@gmail.com>
2022-07-27 14:33:17 +0200

    Only admin and manager redirect to dashboard after login

--- tests/Feature/Auth/AuthenticationTest.php
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -20,17 +20,38 @@ class AuthenticationTest extends TestCase
 
     public function test_users_can_authenticate_using_the_login_screen()
     {
-        $user = User::factory()->create([
+        $user3 = User::factory()->create([
+            'access_level' => 3
+        ]);
+
+        $response3 = $this->post('/login', [
+            'email' => $user3->email,
+            'password' => 'password',
+        ]);
+        
+        $user1 = User::factory()->create([
             'access_level' => 1
         ]);
 
-        $response = $this->post('/login', [
-            'email' => $user->email,
+        $response1 = $this->post('/login', [
+            'email' => $user1->email,
+            'password' => 'password',
+        ]);
+        
+        $user2 = User::factory()->create([
+            'access_level' => 2
+        ]);
+
+
+        $response2 = $this->post('/login', [
+            'email' => $user2->email,
             'password' => 'password',
         ]);
 
         $this->assertAuthenticated();
-        $response->assertRedirect(RouteServiceProvider::HOME);
+        $response1->assertRedirect(RouteServiceProvider::HOME);
+        $response2->assertRedirect(RouteServiceProvider::HOME);
+        $response3->assertRedirect('/');
     }
 
     public function test_users_can_not_authenticate_with_invalid_password()
